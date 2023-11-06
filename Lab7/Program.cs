042fd6ce4a3547d511dcddb44eab295bc819ce26
LefterStud <lefter.school@gmail.com>
2023-11-06 19:35:47 +0200

    Fix Program.cs

--- Lab7/Program.cs
+++ Lab7/Program.cs
@@ -6,10 +6,10 @@ namespace Lab7
     {
         static void Main(string[] args)
         {
-            SparePart part1 = new SparePart("Wheel", 1, 500, 10);
-            SparePart part2 = new SparePart("Wing", 2, 2000, 100);
-            SparePart part3 = new SparePart("Engine", 3, 20000, 1000);
-            SparePart part4 = new SparePart("Сrankshaft", 4, 3000, 5);
+            SparePart part1 = new SparePart("Wheel", 500, 10);
+            SparePart part2 = new SparePart("Wing", 2000, 100);
+            SparePart part3 = new SparePart("Engine", 20000, 1000);
+            SparePart part4 = new SparePart("Crankshaft", 3000, 5);
             Storage storage = new Storage();
             storage.AddPart(part1);
             storage.AddPart(part2);
@@ -20,7 +20,7 @@ namespace Lab7
             {
                 Console.WriteLine(part.ToString());
             }
-            Console.WriteLine("Find parts by Id:");
+            Console.WriteLine("Find parts by Id and later Remove:");
             Console.WriteLine(storage.FindPartById(1));
             storage.RemovePart(storage.FindPartById(1));
             Console.WriteLine("All spare parts:");
